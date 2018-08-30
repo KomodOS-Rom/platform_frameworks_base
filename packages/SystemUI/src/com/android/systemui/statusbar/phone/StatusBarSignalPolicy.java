a66838243b200fa53dcff495734a6d788b05152d
Wang Han <wanghan1995315@gmail.com>
2018-08-30 06:23:37 +0000

    Revert "StatusBarSignalPolicy: Fix missing provisioned in equals and copyTo"
    This reverts commit 7ccc763bf8c794136d6b347574971e9dc04c6b23.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
@@ -424,7 +424,6 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
                     typeId == that.typeId &&
                     roaming == that.roaming &&
                     needsLeadingPadding == that.needsLeadingPadding &&
-                    provisioned == that.provisioned &&
                     Objects.equals(typeContentDescription, that.typeContentDescription) &&
                     volteId == that.volteId;
         }
@@ -450,7 +449,6 @@ public class StatusBarSignalPolicy implements NetworkControllerImpl.SignalCallba
             other.typeId = typeId;
             other.roaming = roaming;
             other.needsLeadingPadding = needsLeadingPadding;
-            other.provisioned = provisioned;
             other.typeContentDescription = typeContentDescription;
             other.volteId = volteId;
         }

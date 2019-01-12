a3b96d5aef05fca0ce6f7fa55211e072fdb5e5c9
Kshitij Gupta <kshitijgm@gmail.com>
2019-01-13 00:51:53 +0100

    Custom Rounded Corner and Padding preferences [1/2]
    RoundedCorners: Use even if resource values are 0
    
    Change-Id: Ib8031f420220cdd19da3ced86c066c2d3282d517
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    Settings: Add Rounded corner strings to Settings.Secure
    
    Change-Id: Ifa158a2138e96dbbed68bae0ff178212b061bc65
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    base: Allow using framework values for rounded corners [1/2]
    
    Change-Id: I246a34f0b52ca595f6bcfd7367631673db25c0cf
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    base: corners: Evaluate framework values correctly [2/2]
    
    ScreenDecorations: corners: Fixes for pie
    
    - If the device has framework value 0dp for corners, it would ideally
      misbehave when we override it with our secure setting.
    - To fix this, restore and modify default hasRoundedCorners logic, add
      SIZE tunable unconditionally, setupDecorations if necessary
      onTuningChanged, and make sure to setVisibility when needed.
    
    Change-Id: I9719ab8e18cda2be4db33fa8d871945b6722e2bd
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    
    StatusBar: corners: Fix dp evaluation
    
    - The secure setting stores the value in dp, not in native px. Divide
      our px value with density to get true dp value.
    - Also, since dp value is stored as an int, use getDimension instead of
      getDimensionPixelSize to  prevent loss during division and rounding
      off.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -2363,6 +2363,34 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
     }
 
+    public boolean isCurrentRoundedSameAsFw() {
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        Resources res = null;
+        try {
+            res = mContext.getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+            // If we can't get resources, return true so that updateCorners doesn't attempt to
+            // set corner values
+            return true;
+        }
+
+        // Resource IDs for framework properties
+        int resourceIdRadius = res.getIdentifier("com.android.systemui:dimen/rounded_corner_radius", null, null);
+        int resourceIdPadding = res.getIdentifier("com.android.systemui:dimen/rounded_corner_content_padding", null, null);
+
+        // Values on framework resources
+        int cornerRadiusRes = (int) (res.getDimension(resourceIdRadius) / density);
+        int contentPaddingRes = (int) (res.getDimension(resourceIdPadding) / density);
+
+        // Values in Settings DBs
+        int cornerRadius = Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.SYSUI_ROUNDED_SIZE, cornerRadiusRes);
+        int contentPadding = Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING, contentPaddingRes);
+
+        return (cornerRadiusRes == cornerRadius) && (contentPaddingRes == contentPadding);
+    }
 
     @Nullable
     public View getAmbientIndicationContainer() {
@@ -4253,6 +4281,30 @@ public class StatusBar extends SystemUI implements DemoMode,
             // Make sure we have the correct navbar/statusbar colors.
             mStatusBarWindowManager.setKeyguardDark(useDarkText);
         }
+        updateCorners();
+    }
+
+    private void updateCorners() {
+        boolean sysuiRoundedFwvals = Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                    Settings.Secure.SYSUI_ROUNDED_FWVALS, 1, UserHandle.USER_CURRENT) == 1;
+        if (sysuiRoundedFwvals && !isCurrentRoundedSameAsFw()) {
+            float density = Resources.getSystem().getDisplayMetrics().density;
+            Resources res = null;
+            try {
+                res = mContext.getPackageManager().getResourcesForApplication("com.android.systemui");
+            } catch (NameNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            if (res != null) {
+                int resourceIdRadius = res.getIdentifier("com.android.systemui:dimen/rounded_corner_radius", null, null);
+                Settings.Secure.putInt(mContext.getContentResolver(),
+                    Settings.Secure.SYSUI_ROUNDED_SIZE, (int) (res.getDimension(resourceIdRadius) / density));
+                int resourceIdPadding = res.getIdentifier("com.android.systemui:dimen/rounded_corner_content_padding", null, null);
+                Settings.Secure.putInt(mContext.getContentResolver(),
+                    Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING, (int) (res.getDimension(resourceIdPadding) / density));
+            }
+        }
     }
 
      // Switches qs tile style back to stock.
@@ -5164,6 +5216,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_TILE_STYLE),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.SYSUI_ROUNDED_FWVALS),
+                    false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.HEADS_UP_STOPLIST_VALUES), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
@@ -5194,6 +5249,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                     // Keeps us from overloading the system by performing these tasks every time.
                     unloadAccents();
                     updateAccents();
+            } else if (uri.equals(Settings.Secure.getUriFor(Settings.Secure.SYSUI_ROUNDED_FWVALS))) {
+                updateCorners();
                 update();
         }
     }
@@ -5204,6 +5261,7 @@ public class StatusBar extends SystemUI implements DemoMode,
             updateNavigationBar();
             setHeadsUpStoplist();
             setHeadsUpBlacklist();
+            updateCorners();
         }
     }
 

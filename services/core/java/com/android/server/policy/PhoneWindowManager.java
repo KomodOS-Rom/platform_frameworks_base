15efd6b13ff21b718a5f4f3b6a682b39d1f445db
Randall Rushing <randall.rushing@gmail.com>
2019-01-16 17:25:09 +0100

    Hardware key disable support

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6608,6 +6608,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
         }
     }
+	
+    private boolean isHwKeysDisabled() {
+        return mKeyHandler != null ? mKeyHandler.isHwKeysDisabled() : false;
+    }
 
     /** {@inheritDoc} */
     @Override
@@ -6699,7 +6703,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         boolean useHapticFeedback = down
                 && (policyFlags & WindowManagerPolicy.FLAG_VIRTUAL) != 0
                 && (!isNavBarVirtKey || mNavBarVirtualKeyHapticFeedbackEnabled)
-                && event.getRepeatCount() == 0;
+                && event.getRepeatCount() == 0
+                && !isHwKeysDisabled();
 
         // Specific device key handling
         if (dispatchKeyToKeyHandlers(event)) {

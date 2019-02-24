be364fddcd015af4d10609263025b3988cd6fbaf
mdeejay <mdjrussia@gmail.com>
2019-02-24 05:01:19 +0100

    SystemUI: Remove DU analog clock

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13785,7 +13785,6 @@ public final class Settings {
          */
         public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
 		
-
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.

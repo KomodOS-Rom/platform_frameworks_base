4ca51ad3de5a2b30c8735105e2299b2b63e89f9b
mdeejay <mdjrussia@gmail.com>
2019-05-25 14:55:01 +0200

    Revert "SystemUI: Add visualizer feature"
    This reverts commit c61d5ed89720c2085a56e162dfff03475ff4ff57.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9722,13 +9722,6 @@ public final class Settings {
          * @hide
          */
         public static final String PULSE_APPS_BLACKLIST = "pulse_apps_blacklist";
-		
-        /**
-         * Lockscreen Visualizer
-         *
-         * @hide
-         */
-        public static final String LOCKSCREEN_VISUALIZER_ENABLED = "lockscreen_visualizer";
 
         /**
          * This are the settings to be backed up.
